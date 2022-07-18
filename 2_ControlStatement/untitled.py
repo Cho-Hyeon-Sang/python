ff3690ec38bdab9820c3e4a22bab088e5facab8c
boyeon <boyeonning@gamil.com>
2022-07-18 22:58:30 +0900

    quiz_3

--- 2_ControlStatement/untitled.py
+++ 2_ControlStatement/untitled.py
@@ -27,3 +27,40 @@ def get_prime_cnt_sum(number):
     return f'찌찌 개수:{len(li)}, 찌찌 합:{sum(li)}'
 
 print(get_prime_cnt_sum(number=6))
+
+
+# quiz_3
+from pprint import pprint
+
+pog_csv = [ {'floor' : 0, 'cell' : 0},
+  {'floor' : 0, 'cell' : 0},
+  {'floor' : 0, 'cell' : 1},
+  {'floor' : 0, 'cell' : 2},
+  {'floor' : 0, 'cell' : 3},
+  {'floor' : 1, 'cell' : 0},
+  {'floor' : 1, 'cell' : 1},
+  {'floor' : 1, 'cell' : 2},
+  {'floor' : 1, 'cell' : 3},
+  {'floor' : 1, 'cell' : 4},
+  {'floor' : 2, 'cell' : 0},
+  {'floor' : 2, 'cell' : 1},
+  {'floor' : 2, 'cell' : 1},
+  {'floor' : 2, 'cell' : 2},
+  {'floor' : 2, 'cell' : 2},]
+
+  # 결과 -> {
+#           '0': {'0': 0, '1': 0, '2': 0, '3': 0}, 
+#           '1': {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0}, 
+#           '2': {'0': 0, '1': 0, '2': 0}
+#        } 
+
+dic ={}
+for i in pog_csv:
+    dic[str(i['floor'])] = {}
+for i in range(len(pog_csv)):
+    each = pog_csv[i]
+    floor = str(each['floor'])
+    cell = str(each['cell'])
+    dic[floor][cell] = 0
+pprint(dic)
+# 이렇게 해도 될까요? 개현상 선생님?
